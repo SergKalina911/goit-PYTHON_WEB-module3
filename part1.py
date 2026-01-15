72259a18b248a13ea609d42c9a6ea65ef5b679ad
Serg Kalynychenko <kalinasergey911@gmail.com>
2026-01-15 02:57:31 +0200

    Event

--- part1.py
+++ part1.py
@@ -564,4 +564,94 @@ worker_two The worker can do the work
 condition.notify.
 
 
+                        Event
+
+​Інший примітив синхронізації — це потокобезпечний прапорець класу Event. Клас Event має внутрішній прапорець, який 
+можуть встановлювати або скидати інші потоки. Для цього використовують метод set, щоб встановити прапор та метод 
+clear для скидання. Методу wait класу Event зупиняє роботу потоку до того часу, доки інший потік не встановить 
+прапор методом set. Є можливість перевірити, чи встановлено прапор методом is_set.
+
+Таким чином, master може встановити прапорець класу Event, і всі worker потоки продовжать роботу тільки після 
+отримання дозволу."""
+
+from threading import Thread, Event
+import logging
+from time import sleep
+
+
+def worker(event: Event):
+    logging.debug('Worker ready to work')
+    event.wait()
+    logging.debug('The worker can do the work')
+
+
+def master(event: Event):
+    logging.debug('Master doing some work')
+    sleep(2)
+    logging.debug('Informing that workers can do the work')
+    event.set()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    event = Event()
+    master = Thread(name='master', target=master, args=(event, ))
+
+    worker_one = Thread(name='worker_one', target=worker, args=(event, ))
+    worker_two = Thread(name='worker_two', target=worker, args=(event,))
+    worker_one.start()
+    worker_two.start()
+    master.start()
+
+    logging.debug('End program')
+
+"""
+Виведення буде таке саме, як і для Condition:
+
+worker_one Worker ready to work
+worker_two Worker ready to work
+master Master doing some work
+MainThread End program
+master Informing that workers can do the work
+worker_one The worker can do the work
+worker_two The worker can do the work
+
+Виникає закономірне питання, навіщо, якщо результат той самий? Справа в тому, що ми можемо керувати виконанням, 
+перезапуском та зупинкою роботи потоків через клас Event. Наприклад, у наступному прикладі ми перериваємо 
+виконання потоку, який працює в нескінченному циклі та інакше просто ніколи не завершиться."""
+
+from threading import Thread, Event
+import logging
+from time import sleep
+
+
+def example_work(event_for_exit: Event):
+    while True:
+        sleep(1)
+        logging.debug('Run event work')
+
+        if event_for_exit.is_set():
+            break
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    event = Event()
+    thread = Thread(target=example_work, args=(event,))
+    thread.start()
+
+    sleep(5)
+    event.set()
+
+    logging.debug('End program')
+
+"""
+Виведення:
+
+Thread-1 Run event work
+Thread-1 Run event work
+Thread-1 Run event work
+Thread-1 Run event work
+MainThread End program
+Thread-1 Run event work
 """
