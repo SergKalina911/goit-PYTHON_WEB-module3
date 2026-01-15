836e89de7900d940ba6d3f3d90f6aabbce61d9d9
Serg Kalynychenko <kalinasergey911@gmail.com>
2026-01-15 02:17:22 +0200

    Очікування виконання потоку

--- part1.py
+++ part1.py
@@ -220,8 +220,90 @@ Thread-3 Wake up!
 Thread-4 Wake up!
 Thread-5 Wake up!
 
-
 Зверніть увагу, що аргументи, які потрібно передати у функцію, передаються як кортеж args у Thread. Іменовані 
 аргументи для функції можна так само передати як словник kwargs у Thread.
+
+
+                        Очікування виконання потоку
+
+​Коли потрібно в основному застосунку дочекатися виконання потоку, можна скористатися блокуючим методом join."""
+
+from threading import Thread
+import logging
+from time import sleep
+
+
+def example_work(params):
+    sleep(params)
+    logging.debug('Wake up!')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    logging.debug('Start program')
+    threads = []
+    for i in range(5):
+        thread = Thread(target=example_work, args=(i,))
+        thread.start()
+        threads.append(thread)
+
+    [el.join() for el in threads]
+
+    logging.debug('End program')
+
+"""
+У консолі ви побачите:
+
+MainThread Start program
+Thread-1 Wake up!
+Thread-2 Wake up!
+Thread-3 Wake up!
+Thread-4 Wake up!
+Thread-5 Wake up!
+MainThread End program
+
+Основний потік дочекався [el.join() for el in threads], доки завершаться всі потоки thread зі списку threads, і 
+тільки потім вивів End program.
+
+Ви також можете перевірити — чи виконується потік, викликавши метод is_alive:"""
+
+from threading import Thread
+from time import sleep
+import logging
+
+
+class UsefulClass:
+    def __init__(self, second_num):
+        self.delay = second_num
+
+    def __call__(self):
+        sleep(self.delay)
+        logging.debug('Wake up!')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    t2 = UsefulClass(2)
+    thread = Thread(target=t2)
+    thread_locking = Thread(target=t2)
+
+    thread.start()
+    print(thread.is_alive(), thread_locking.is_alive())
+    thread_locking.start()
+    thread.join()
+    thread_locking.join()
+    print(thread.is_alive(), thread_locking.is_alive())
+    print('After all...')
+
+"""
+True False
+Thread-2 Wake up!
+Thread-1 Wake up!
+False False
+After all...
+
+Це може бути корисним, якщо ви хочете перевіряти стан потоку самостійно і не блокувати застосунок в очікуванні 
+завершення.
+
 
 """
