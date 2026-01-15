7a349ed6f92f62a6c09069277faabfac452417c8
Serg Kalynychenko <kalinasergey911@gmail.com>
2026-01-15 02:10:18 +0200

    Потік як функтор

--- part1.py
+++ part1.py
@@ -157,5 +157,35 @@ Thread-4 args: ('Count thread - 3',)
 потоків MyThread вивели своє 'Wake up!', і тільки після цього скрипт завершився.
 
 
+                        Потік як функтор
 
+​Є інший спосіб виконати код окремого потоку. Для цього потрібно, щоб код виконання був функтором (функцією або 
+класом, який має метод __call__). Тоді об'єкт можна передати як іменований аргумент target у Thread:
 """
+
+from threading import Thread
+from time import sleep
+import logging
+
+
+class UsefulClass():
+    def __init__(self, delay_time):
+        self.delay = delay_time
+
+    def __call__(self):
+        sleep(self.delay)
+        logging.debug('Wake up!')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    t2 = UsefulClass(2)
+    thread = Thread(target=t2)
+    thread.start()
+    print('Some stuff')
+
+# Результат:
+# Some stuff
+# Thread-1 Wake up!
+
+""" """
