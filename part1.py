29c348e0f3b26a3a8ea6e6bfc5653f4c1cc2d8a8
Serg Kalynychenko <kalinasergey911@gmail.com>
2026-01-15 02:21:52 +0200

    Потоки Timer

--- part1.py
+++ part1.py
@@ -306,4 +306,45 @@ After all...
 завершення.
 
 
+                        Потоки Timer
+
+​Екземпляри класу Timer починають працювати з деякою затримкою, яку визначає програміст. Крім того, ці потоки можна 
+скасувати будь-якої миті в період затримки. Наприклад, ви передумали стартувати певний потік."""
+
+from threading import Timer
+import logging
+from time import sleep
+
+
+def example_work():
+    logging.debug('Start!')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+
+    first = Timer(0.5, example_work)
+    first.name = 'First thread'
+    second = Timer(0.7, example_work)
+    second.name = 'Second thread'
+    logging.debug('Start timers')
+    first.start()
+    second.start()
+    sleep(0.6)
+    second.cancel()
+
+    logging.debug('End program')
+
+"""
+Тут ми запланували виконання двох потоків, через 0.5 та 0.7 секунд. Але потім через 0.6 секунди скасували 
+виконання другого потоку second.cancel()
+
+Отримаємо таке виведення:
+
+MainThread Start timers
+First thread Start!
+MainThread End program
+
+
+
 """
