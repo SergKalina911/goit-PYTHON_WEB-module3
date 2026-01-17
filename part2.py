92576fb8cc64a0b2c44d141886f7d9cb1f68a955
Serg Kalynychenko <kalinasergey911@gmail.com>
2026-01-17 13:57:33 +0200

    Pipe (канал)

--- part2.py
+++ part2.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     [el.join() for el in processes]
     # Виведення кодів завершення процесів
     [print(el.exitcode, end=' ') for el in processes]
-    logger.debug('End program')
+logger.debug('End program')
 
 """
 Виведення буде наступним:
@@ -242,7 +242,7 @@ from multiprocessing import Process, RLock, current_process
 from multiprocessing.sharedctypes import Value, Array
 from ctypes import Structure, c_double
 import logging
-sleep(5)
+# sleep(5)
 # Налаштування логування
 logger = logging.getLogger()
 # встановлення обробника виведення в консоль
@@ -352,7 +352,7 @@ from time import sleep
 import logging
 # затримка для встигнення підключитися до консолі
 # print( "You have 5 seconds to connect to the console..." )
-sleep(5)
+# sleep(5)
 # Налаштування логування
 logger = logging.getLogger()
 stream_handler = logging.StreamHandler()
@@ -422,3 +422,88 @@ End program
 https://docs.python.org/3.5/library/multiprocessing.html#multiprocessing.managers.SyncManager.list . 
 
        
+                        Pipe (канал)
+
+Механізм каналів реалізований поверх сокетів (мережеві інтерфейси) і дає зручніший інтерфейс. Канали доступні на 
+всіх POSIX системах (Linux, Mac OS, FreeBSD, Unix).
+
+Канал — пара об'єктів — кінці каналу. Ви можете асинхронно писати в канал (надсилати повідомлення) та читати з 
+каналу (отримувати повідомлення на іншому кінці). Повідомлень у каналі може бути більше одного одночасно і 
+порядок повідомлень гарантується. Канали реалізують обмін між двома процесами, не можна використовувати один і 
+той самий кінець каналу в кількох процесах.
+
+Клас Pipe модуля multiprocessing повертає парний кортеж (conn1, conn2), що складається з об'єктів Connection, 
+що представляють кінці одного каналу.
+
+Клас Pipe приймає аргумент дуплекс duplex, який за замовчуванням дорівнює True. Тоді канал є двоспрямованим, 
+якщо ж duplex=False, тоді канал є односпрямованим і тоді conn1 використовується для приймання повідомлень, а 
+conn2 для надсилання повідомлень.
+
+Об'єкти conn1 та conn2 мають ряд методів, основні це:
+- send — відправляє об'єкт на інший кінець з'єднання
+- recv — повертає об'єкт, відправлений з іншого кінця з'єднання
+- close закриває з'єднання
+
+
+Давайте розглянемо приклад реалізації каналу в Python за допомогою класу Pipe."""
+
+from multiprocessing import Pipe, Process, current_process
+from time import sleep
+import sys
+import logging
+# Логування налаштування
+logger = logging.getLogger()
+stream_handler = logging.StreamHandler()
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+# створення каналу 
+recipient1, sender1 = Pipe()
+recipient2, sender2 = Pipe()
+
+# Функція, яка буде виконуватися в окремому процесі
+def worker(pipe: Pipe):
+    # Отримання імені поточного процесу
+    name = current_process().name
+    # Логування початку роботи процесу
+    logger.debug(f'{name} started...')
+    # Отримання значення з каналу
+    val = pipe.recv()
+    # Логування отриманого значення
+    logger.debug(f'{name} received: {val}')
+    # Логування квадрата отриманого значення
+    logger.debug(val**2)
+    # Завершення процесу
+    sys.exit(0)
+
+# Головний блок виконання
+if __name__ == '__main__':
+    # Створення процесів
+    w1 = Process(target=worker, args=(recipient1, ))
+    w2 = Process(target=worker, args=(recipient2, ))
+# Запуск процесів
+    w1.start()
+    w2.start()
+# Надсилання значень у канали
+    sender1.send(8)
+    sleep(1)
+    sender2.send(16)
+# Очікування завершення процесів
+    w1.join()
+    w2.join()
+    # логування завершення програми
+    logger.debug('End program')
+
+""" 
+У цьому прикладі ми пишемо в канали з батьківського процесу sender1.send(8) та sender2.send(16), а читаємо з 
+дочірніх pipe.recv() всередині функції worker.
+
+Виведення:
+
+Process-1 started...
+64
+Process-2 started...
+256
+
+У канал можна відправити будь-який тип даних, який можна перетворити на byte-рядок за допомогою pickle. 
+Не можна надсилати функції або інші об'єкти, які не серіалізуються.
+"""
